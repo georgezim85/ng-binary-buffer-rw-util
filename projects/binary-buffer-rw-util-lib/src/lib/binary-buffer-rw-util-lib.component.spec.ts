a48c003c2fcce399da7dcbf357ffbd96fbe1f514
georgezim85 <devgeo85@gmail.com>
2021-04-21 01:07:07 -0400

    Creates functions: convertDec2binString, dec2bin, bin2dec, convertUint8Array2Number.

--- projects/binary-buffer-rw-util-lib/src/lib/binary-buffer-rw-util-lib.component.spec.ts
+++ projects/binary-buffer-rw-util-lib/src/lib/binary-buffer-rw-util-lib.component.spec.ts
@@ -22,4 +22,39 @@ describe('BinaryBufferRwUtilLibComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('convert decimal to binary string', () => {
+    // e.g. decimal 1616165571   =>  binary 1100000010101001011101011000011
+    // console.log(byteArray);    // Uint8Array{0: 192, 1: 169, 2: 117, 3: 67
+    expect(component.convertDec2binString(192)).toEqual('11000000');
+    expect(component.convertDec2binString(169)).toEqual('10101001');
+    expect(component.convertDec2binString(117)).toEqual('01110101');
+    expect(component.convertDec2binString(67)).toEqual( '01000011');
+
+    expect(component.convertDec2binString(1616165571)).toEqual('1100000010101001011101011000011');
+  });
+
+  it('convert Uint8 (binary format) to binary string', () => {
+    expect(component.convertDec2binString(0b11000000)).toEqual('11000000');
+    expect(component.convertDec2binString(0b10101001)).toEqual('10101001');
+    expect(component.convertDec2binString(0b01110101)).toEqual('01110101');
+    expect(component.convertDec2binString(0b01000011)).toEqual('01000011');
+  });
+
+  it('convert Uint8Array to number', () => {
+    let byteArray = new Uint8Array(4);
+    byteArray[0] = 0b01100000;
+    byteArray[1] = 0b01010100;
+    byteArray[2] = 0b10111010;
+    byteArray[3] = 0b11000011;
+    expect(component.convertUint8Array2Number(byteArray)).toEqual(1616165571);
+  });
+
+  it('convert binary to decimal string', () => {
+    expect(component.bin2dec('1110101')).toEqual('117');
+  });
+
+  it('convert decimal to binary string', () => {
+    expect(component.dec2bin(117)).toEqual('1110101');
+  });
 });
--- projects/binary-buffer-rw-util-lib/src/lib/binary-buffer-rw-util-lib.component.ts
+++ projects/binary-buffer-rw-util-lib/src/lib/binary-buffer-rw-util-lib.component.ts
@@ -17,4 +17,26 @@ export class BinaryBufferRwUtilLibComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // convertDec2binString(1616165571) results '1100000010101001011101011000011'
+  convertDec2binString(number: number) {
+    return this.dec2bin(number).padStart(8, '0');
+  }
+
+  dec2bin(dec: number){
+    return (dec >>> 0).toString(2);
+  }
+
+  bin2dec(bin: string){
+    return parseInt(bin, 2).toString(10);
+  }
+
+  convertUint8Array2Number(byteArray: Uint8Array): number {
+    let number = 0;
+    for (let i = 0; i < byteArray.length; i++) {
+      number <<= 8;
+      number |= byteArray[i];
+    }
+    return number;
+  }
+
 }
